c0fb4e29a6d4dc06ee7efb9ee954d00b74f32520
quick007 <69743171+quick007@users.noreply.github.com>
2025-02-19 18:35:35 -0800

    actually save settings in zustand

--- src/renderer/stores/settings.ts
+++ src/renderer/stores/settings.ts
@@ -1,8 +1,6 @@
 import { Settings } from "@/main/ipc/_shared/settings";
 import { create } from "zustand";
 
-
-
 interface SettingsStore {
 	settings: Settings;
 	fetch: () => Promise<void>;
@@ -10,10 +8,14 @@ interface SettingsStore {
 
 export const useSettingsStore = create<SettingsStore>()((set) => ({
 	// settings won't be null by the time it's used as a loading screen is displayed while fetch is being called
-		settings: null,
-		fetch: async () => {
-			const test = await window.electronAPI.fetchSettings()
-			console.log(test)
-			return;
+	settings: null,
+	fetch: async () => {
+		const { data: settings, error } = await window.electronAPI.fetchSettings();
+		if (error) {
+			// in the future there will be proper error reporting
+			console.error(error);
+		} else {
+			set({ settings });
 		}
-}))
+	},
+}));
